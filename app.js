781df8a88909506434cb5fb91f95b57f04024818
Rajveer Mistri <rajveermistri63@gmail.com>
2026-08-27 20:39:17 +0530

    update application message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello! CI/CD pipeline is Working!");
+    res.send("Hello! CI/CD pipeline is Working Successfully!");
 });
 
 app.listen(PORT, () => {
